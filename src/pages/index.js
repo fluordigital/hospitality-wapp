631b84f5177ab29b8edaf9a8d3c594569a9ff4ae
Fernando Mendonca <fluordigital@gmail.com>
2021-03-06 19:04:59 -0300

    config Material Ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
       Hospitality Web App! Com Next.js e Material Ui
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+      <Button variant="contained" color="primary" disableElevation>
+        Disable elevation
+      </Button>
     </div>
   );
 }
